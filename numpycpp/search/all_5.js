a20e33f9fa5934cad4ad38f722ee27b52fd30fc1
Michael Welter <michael@welter-4d.de>
2016-01-27 10:05:52 +0100

    numpy doc update

--- numpycpp/search/all_5.js
+++ numpycpp/search/all_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['from_5farrayt',['from_arrayt',['../structboost_1_1python_1_1numpy_1_1mw__py__impl_1_1from__arrayt.html',1,'boost::python::numpy::mw_py_impl']]],
   ['from_5farraytbase',['from_arraytbase',['../structboost_1_1python_1_1numpy_1_1mw__py__impl_1_1from__arraytbase.html',1,'boost::python::numpy::mw_py_impl']]],
   ['from_5fnumpy_5fscalar',['from_numpy_scalar',['../structboost_1_1python_1_1numpy_1_1mw__py__impl_1_1from__numpy__scalar.html',1,'boost::python::numpy::mw_py_impl']]]
 ];
--- numpycpp/search/classes_3.js
+++ numpycpp/search/classes_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['to_5fnumpyarrayt',['to_numpyarrayt',['../structboost_1_1python_1_1numpy_1_1mw__py__impl_1_1to__numpyarrayt.html',1,'boost::python::numpy::mw_py_impl']]],
+  ['to_5farrayt',['to_arrayt',['../structboost_1_1python_1_1numpy_1_1mw__py__impl_1_1to__arrayt.html',1,'boost::python::numpy::mw_py_impl']]],
   ['to_5ftoarraytbase',['to_toarraytbase',['../structboost_1_1python_1_1numpy_1_1mw__py__impl_1_1to__toarraytbase.html',1,'boost::python::numpy::mw_py_impl']]]
 ];
